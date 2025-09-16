3ff2fda1b6a33841287b8358b445eebce27cd2f7
aquaFV <118510313+aquaFV@users.noreply.github.com>
2025-09-16 17:55:12 +0300

    Restarted.

--- Managers/SortingManager/SortingManager.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include "../StudentManager/StudentClass.h"
-
-namespace SortingManager {
-    void sort_students(std::vector<Student>& students, const std::string& orderBy,
-                       const std::string& order);
-}
